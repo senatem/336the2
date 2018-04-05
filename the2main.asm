63d1f00b561643dfa59172a06907070f43d9f907
Sena Temuçin <sena.temucin@metu.edu.tr>
2018-04-05 16:31:55 +0300

    Added button tasks

--- the2main.asm
+++ the2main.asm
@@ -1,10 +1,7 @@
 #include "p18f8722.inc"
 
-UDATA_ACS
-  t1	res 1	; used in delay
-  t2	res 1	; used in delay
-  t3	res 1	; used in delay
-
+    ;variable but0,but1,but2,but3
+    
 ORG    0x00            ; processor reset vector
     GOTO    INIT                  ; go to beginning of program
     
@@ -35,12 +32,23 @@ INIT
     MOVWF PORTH
     MOVLW 0x3F
     MOVWF PORTJ
-    MOVLW 0xC3
-    MOVWF T0CON
-    MOVLW 0x45
-    MOVWF TMR0L
-    BSF INTCON,7
-    BSF INTCON,5
+    
+    ;Disable interrupts
+    clrf    INTCON
+    clrf    INTCON2
+
+
+    ;Initialize Timer0
+    movlw   b'01000111' ;Disable Timer0 by setting TMR0ON to 0 (for now)
+                        ;Configure Timer0 as an 8-bit timer/counter by setting T08BIT to 1
+                        ;Timer0 increment from internal clock with a prescaler of 1:256.
+    movwf   T0CON ; T0CON = b'01000111'
+
+    ;Enable interrupts
+    movlw   b'11100000' ;Enable Global, peripheral, Timer0 and RB interrupts by setting GIE, PEIE, TMR0IE and RBIE bits to 1
+    movwf   INTCON
+
+    bsf     T0CON, 7    ;Enable Timer0 by setting TMR0ON to 1
 
 MAIN
     CALL BUTTON1
@@ -49,15 +57,70 @@ MAIN
     CALL BUTTON4
     GOTO MAIN
 
-BUTTON1
+BUTTON0
     ; Button Task for RG0
-BUTTON2
+    BTFSS PORTG,0
+    goto _isoff
+    goto _ison
+    _isoff:
+	BTFSS but0,0
+	return
+	;move right paddle down
+	BCF but0,0
+    _ison:
+	BTFSS but0,0
+	BSF but0,0
+	return
+  
+
+BUTTON1
     ; Button Task for RG1
-BUTTON3
+    BTFSS PORTG,1
+    goto _isoff
+    goto _ison
+    _isoff:
+	BTFSS but1,0
+	return
+	;move right paddle up
+	BCF but1,0
+    _ison:
+	BTFSS but1,0
+	BSF but1,0
+	return
+BUTTON2
     ; Button Task for RG2
-BUTTON4
-    ; Button Task for RG3
+    BTFSS PORTG,2
+    goto _isoff
+    goto _ison
+    _isoff:
+	BTFSS but2,0
+	return
+	;move left paddle down
+	BCF but2,0
+    _ison:
+	BTFSS but2,0
+	BSF but2,0
+	return
+BUTTON3
+    ; Button Task for RG0
+    BTFSS PORTG,3
+    goto _isoff
+    goto _ison
+    _isoff:
+	BTFSS but3,0
+	return
+	;move left paddle up
+	BCF but3,0
+    _ison:
+	BTFSS but3,0
+	BSF but3,0
+	return
 ISR
+    MOVLW 0x45
+    MOVWF TMR0L
+    MOVLW 0x0F
+    MOVWF PORTA
+    RETFIE
     ; Reset Timer
     ; Move Ball
     ; Check Score
